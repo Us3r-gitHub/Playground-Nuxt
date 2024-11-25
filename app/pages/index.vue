a6f34a5e93f8a6f53adce4f5b02c497bf7a398f5
Us3r <chapsaplay3r@gmail.com>
2024-11-25 08:23:45 +0000

    ✨ feat(auto-form): add variant to disable input

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,23 +1,14 @@
 <script setup lang="ts">
 import { h } from 'vue'
 import * as z from 'zod'
+import { DependencyType } from '~/components/ui/auto-form/interface'
 import { toast } from '~/components/ui/toast';
 
 const schema = z.object({
-  name: z
-    .string({
-      required_error: 'Name is required.',
-    })
-    .min(2, {
-      message: 'Name must be at least 2 characters.',
-    }),
-  username: z
-    .string({
-      required_error: 'User Name is required.',
-    })
-    .min(2, {
-      message: 'User Name must be at least 2 characters.',
-    }),
+  firstName: z.string({ description: "First Name" }).default("first name"),
+  lastName: z.string({ description: "Last Name" }).default("last name"),
+  fullName: z.string({ description: "Full Name" }).default("full name"),
+  userName: z.string({ description: "User Name" }).default("user name"),
 })
 
 function onSubmit(values: Record<string, any>) {
@@ -32,12 +23,32 @@ function onSubmit(values: Record<string, any>) {
   <Card>
     <CardContent class="tw-p-0">
       <AutoForm class="w-2/3 space-y-6" :schema :field-config="{
-        username: {
+        firstName: {
+          description: 'Disabled by dependency configuration',
+        },
+        lastName: {
+          description: 'Disabled using component slot',
+        },
+        fullName: {
+          description: 'Disabling using inputProps option in field config',
           inputProps: {
             disabled: true,
           },
         },
-      }" @submit="onSubmit">
+        userName: {
+          description: 'Not Disabled',
+        },
+      }" :dependencies="[
+        {
+          sourceField: 'firstName',
+          targetField: 'firstName',
+          type: DependencyType.DISABLES,
+          when: () => true
+        }
+      ]" @submit="onSubmit">
+        <template #lastName="slotProps">
+          <AutoFormFieldInput v-bind="slotProps" disabled />
+        </template>
         <Button type="submit">
           Submit
         </Button>
