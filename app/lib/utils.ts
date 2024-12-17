e2fd8ba736d783e34610fc2c8ca0146c84a527fa
Us3r <chapsaplay3r@gmail.com>
2024-12-17 07:08:04 +0000

    💡 chore: add comment note for cn function import usage
    This commit adds a note to the comment, clarifying that the cn function should be imported with the ~/ prefix instead of @/ if used.
    
    [related-issue](https://github.com/unovue/shadcn-vue/issues/903)

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -3,6 +3,8 @@ import { extendTailwindMerge } from "tailwind-merge";
 
 const twMerge = extendTailwindMerge({ prefix: "tw-" });
 
+// NOTE: To use this `cn` function instead of the generated one,
+// import with the `~/` prefix instead of `@/`.
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
